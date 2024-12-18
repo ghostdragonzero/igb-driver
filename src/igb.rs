0b90efac0e22302ee682750e960f389b42db0cb3
ghostdragonzero <1109648848@qq.com>
2024-12-18 10:06:18 +0800

    test

--- src/igb.rs
+++ src/igb.rs
@@ -116,15 +116,19 @@ impl Igb {
         mii_reg &= !(1<<11);
         self.phy_write(0, mii_reg);
         mii_reg = self.phy_read(0);
-        mii_reg |= 1<<9 | 1<< 12;
+        mii_reg |= 1<<9;
         self.phy_write(0, mii_reg);
+        mii_reg = self.phy_read(0);
+        mii_reg |= 1<<12;
         info!("finnal mii {:b}", mii_reg);
         self.set_flags32(IGB_CTRL, IGB_CTRL_SLU);
         //FRCSPD defaut is 0 FRCDPLX
         self.set_flags32(IGB_CTRL_EXT, IGB_CTRL_EXT_DRV_LOAD);
+        info!("set SLU ok");
         loop{
             let status = self.get_reg32(IGB_STATUS);
-            if (status &(1 << 1)) == (1<<1){
+            info!("status {:b}", status);
+            if (status &(1 << 2)) == (1<<2){
                 break;
             }
         }
